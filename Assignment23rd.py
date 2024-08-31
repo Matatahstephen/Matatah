005d35b2e4ade0b5e2c8f5f51a128b50e316423c
Matatahstephen <matatahstephen3930@gmail.com>
2024-08-31 06:42:20 +0300

    Add files via upload

---
+++ Assignment23rd.py
@@ -0,0 +1,131 @@
+#1
+"""
+name="Matatah"
+age=24
+is_student=False
+print(name)
+print(age)
+print(is_student)
+"""
+
+#2
+"""
+#creating variables
+a=34
+b=133
+#performing operations
+print(a+b) 
+print(b-a)
+print(a*b) 
+print(b/a)
+
+#converting answers to complex numbers
+
+w=int(167)
+x=int(99)
+y=int(4522)
+z=float(3.912)
+
+a=print(complex(w))
+b=print(complex(x))
+c=print(complex(y))
+d=print(complex(z))
+"""
+
+#3
+"""
+#creating string variables
+name="Matatah"
+greet="welcome to python"
+ful_greeting=(name+" "+greet)
+print(ful_greeting)
+#printing length
+print(len(ful_greeting))
+#converting to uppercase
+upper_greeting=ful_greeting.upper()
+print(upper_greeting)
+"""
+
+#4
+"""
+#creating a list
+favorite_colors=["green", "yellow", "blue", "orange", "white", "black"]
+
+#printing frist and last
+print(favorite_colors[0])
+print(favorite_colors[-1])
+
+#adding a new color
+favorite_colors.append("red")
+print(favorite_colors)
+
+#removing the 3rd color from list
+favorite_colors.pop(2)
+print(favorite_colors)
+
+#Making a new list with colors of flag
+favorite_colors.clear()
+print(favorite_colors)
+flag_colors=["black", "red", "green", "white"]
+favorite_colors.extend(flag_colors)
+print(favorite_colors)
+"""
+
+#5
+"""
+#cresting a dictionary
+student_info={"name":"matata", "age": 24, "courses":["python" ,"statistics", "insurance"]}
+#getting values
+x=student_info.get("courses")
+print(x)
+#adding  items to the dictionary
+student_info["year"]=4
+print(student_info)
+"""
+#OVERALL TIMING
+print("it took me 1hr 15 minutes to do this ")
+
+
+
+
+
+
+
+
+
+
+
+s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
